762964ee1659c46ec08b6733b724c4614017bfcf
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-01-02 20:17:57 +0100

    frontend init

--- backend/src/app/article/urls.py
+++ backend/src/app/article/urls.py
@@ -7,7 +7,7 @@ app_name = "articles"
 
 
 urlpatterns = [
+    #POST don't work if the url didn't end with /
     path('', ArticleView.as_view()),
     path('<int:id>', ArticleView.as_view()),
-    # path('/<int:id>', ArticleView.as_view()),
 ]
---
+++ frontend/src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+    <h1>TEST</h1>
+  );
+}
+
+export default App;
