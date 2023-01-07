d2eb63b816bf48fbd28528d2639e143a5f0c7a95
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-01-07 19:50:30 +0100

    delete WIP

--- frontend/src/components/Article/ArticleList.jsx
+++ frontend/src/components/Article/ArticleList.jsx
@@ -1,19 +1,21 @@
-import React from "react";
+import React, { useState } from "react";
 import ArticleCard from './ArticleCard';
-import { useArticles } from '../../hooks/useArticle';
+import { useArticles, useDeleteArticle } from '../../hooks/useArticle';
 
 export default function ArticleList() {
+    // const [articles, setArticles] = useState([]);
+    // useArticles(setArticles);
+    const { articles, setArticles } = useArticles();
 
     function delete_article(id) {
-        console.log(id, 'DELETE');
+        useDeleteArticle(id);
+        setArticles(articles.filter(item => item.id !== id));
     }
-
 
     function update_article(id) {
         console.log(id, 'UPDATE');
     }
 
-    const articles = useArticles();
     const articlesHTML = articles.length > 0 ? (articles.map((article) => (
         <ArticleCard article={article} key={article.id} update_article={() => update_article(article.id)} delete_article={() => delete_article(article.id)} />
     ))) : (<p>No Articles</p>)
--- frontend/src/hooks/useArticle.js
+++ frontend/src/hooks/useArticle.js
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react"
 import ArticleService from '../services/ArticleService';
 
 export function useArticles() {
+    console.log('useArticles');
     const [articles, setArticles] = useState([]);
 
     useEffect(function () {
@@ -9,5 +10,20 @@ export function useArticles() {
             .then(res => setArticles(res.data.data))
             .catch(e => console.error(e));
     }, []);
-    return articles;
+    return { articles, setArticles };
+}
+
+
+// export function useArticles(setArticles) {
+//     useEffect(function () {
+//         ArticleService.GetArticles()
+//             .then(res => setArticles(res.data.data))
+//             .catch(e => console.error(e));
+//     }, []);
+// }
+
+export function useDeleteArticle(id) {
+    ArticleService.DeleteArticle(id)
+        .then(res => console.log(res.status, 'DELETE', id))
+        .catch(e => console.log(e))
 }
