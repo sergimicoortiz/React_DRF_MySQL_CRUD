5302997824410aa7a2bd8455f88a92d64d601581
Sergi Micó Ortiz <sergimicoortiz@gmail.com>
2023-01-13 17:10:20 +0100

    update OK

--- frontend/src/components/Article/ArticleUpdate.jsx
+++ frontend/src/components/Article/ArticleUpdate.jsx
@@ -1,29 +1,23 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { useParams } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
-import { toast } from 'react-toastify';
-import { useUpdateArticle, useArticles } from "../../hooks/useArticle";
+// import { useNavigate } from "react-router-dom";
+// import { toast } from 'react-toastify';
+import { useArticles } from "../../hooks/useArticle";
 import ArticleForm from "./ArticleForm";
 
 export default function ArticleUpdate() {
-    const navigate = useNavigate();
-    const { useOneArticle, articles } = useArticles();
+    // const navigate = useNavigate();
     const { slug } = useParams()
-    useOneArticle(slug);
+    const { useOneArticle, articles, useUpdateArticle } = useArticles();
 
-    function updateArticle(data) {
-        useUpdateArticle(slug, data)
-            .then(ok => {
-                if (ok) {
-                    toast.success('Article updated');
-                    navigate('/article');
-                }
-            });
-    }
+    useEffect(() => {
+        useOneArticle(slug);
+    }, []);
+    
     return (
         <div>
             <p>UPDATE Article: {slug}</p>
-            <ArticleForm SendData={updateArticle} article={articles[0]} />
+            <ArticleForm SendData={(data) => useUpdateArticle(slug, data)} article={articles[0]} />
         </div>
     )
 }
--- frontend/src/hooks/useArticle.js
+++ frontend/src/hooks/useArticle.js
@@ -1,9 +1,11 @@
 import { useState, useEffect, useCallback } from "react"
 import ArticleService from '../services/ArticleService';
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 
 export function useArticles() {
+    const navigate = useNavigate();
     const [articles, setArticles] = useState([]);
-    const [status, setStatus] = useState(false);
 
     useEffect(function () {
         ArticleService.GetArticles()
@@ -11,56 +13,41 @@ export function useArticles() {
             .catch(e => console.error(e));
     }, []);
 
-    const timeoutStatusOK = (time = 400) => {
-        setStatus(true);
-        setTimeout(() => {
-            setStatus(false);
-        }, time);
-    }//intervalStatus
-
     const useDeleteArticle = (slug) => {
         ArticleService.DeleteArticle(slug)
             .then(res => {
                 if (res.status === 200) {
                     setArticles(articles.filter(item => item.slug !== slug));
-                    timeoutStatusOK()
+                    toast.success('Article deleted');
                 };
             })
             .catch(e => console.error(e));
     }//useDeleteArticle
 
-    const useOneArticle = useCallback(slug => {
+    const useOneArticle = useCallback((slug) => {
         console.log('a');
         ArticleService.GetArticle(slug)
             .then(res => setArticles([res.data.data]))
             .catch(e => console.error(e));
     }, []);//useOneArticle
 
-    // const useOneArticle = slug => {
-    //     console.log('a');
-    //     useEffect(() => {
-    //         ArticleService.GetArticle(slug)
-    //             .then(res => setArticles([res.data.data]))
-    //             .catch(e => console.error(e));
-    //     }, []);
-    // }
+    const useUpdateArticle = useCallback((slug, data) => {
+        ArticleService.UpdateArticle(slug, { 'article': data })
+            .then(res => {
+                if (res.status === 200) {
+                    toast.success('Article updated');
+                    navigate('/article');
+                }
+            })
+            .catch(e => console.error(e));
+    }, []);
 
-    return { articles, status, useDeleteArticle, useOneArticle };
+    return { articles, useDeleteArticle, useOneArticle, useUpdateArticle };
 }
 
 export async function useCreateArticle(data) {
     try {
         await ArticleService.CreateArticles({ 'article': data });
-        return true;
-    } catch (error) {
-        console.error(error);
-        return false;
-    }
-}
-
-export async function useUpdateArticle(slug, data) {
-    try {
-        await ArticleService.UpdateArticle(slug, { 'article': data });
         return true;
     } catch (error) {
         console.error(error);
